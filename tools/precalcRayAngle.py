8c08cdfa75083f59e3e6b4a65b965abc883902d7
Jean-Baptiste PERIN <jean-baptiste.perin@orange.com>
2021-01-20 21:25:07 +0100

    compute column index from angle

--- tools/precalcRayAngle.py
+++ tools/precalcRayAngle.py
@@ -1,4 +1,5 @@
 import math
+import codegen
 
 def toFixAngle(angle_radian):
     return angle_radian*128/math.pi
@@ -11,7 +12,8 @@ def rayAngles (fov_angle_radian, number_of_slice):
     for ii in range (number_of_slice):
         k               = number_of_slice - 1 - 2*ii
         currentAngle    = math.atan (k*screenIncrementLength/2)
-        yield currentAngle
+        yield round(currentAngle*128/math.pi)
+
 
 def rayAngle2Col (ray_angle_radian, fov_angle_radian, number_of_slice):
 
@@ -23,22 +25,19 @@ def rayAngle2Col (ray_angle_radian, fov_angle_radian, number_of_slice):
     else:
         return int (number_of_slice//2 - (math.tan(ray_angle_radian)-screenIncrementLength/2)/screenIncrementLength)
 
-
 def main ():
     
-    FOV_IN_DEGREES          = 20
-
-    NUMBER_OF_SLICE         = 5
-
-    tabAngles               = list(map(math.degrees,rayAngles(math.radians(FOV_IN_DEGREES), NUMBER_OF_SLICE)))
-
-    print (tabAngles)
-    tabCol                  = []
-    for angle in range (round(tabAngles[0]), round(tabAngles[-1])-1, -1):
-        idxCol = rayAngle2Col (angle*math.pi/180, math.radians(FOV_IN_DEGREES), NUMBER_OF_SLICE)
-        # print (angle, idxCol, tabAngles[idxCol])
-        tabCol.append(idxCol)
-    print (tabCol)
+    FOV_IN_DEGREES          = 112
+    NUMBER_OF_SLICE         = 40
+    tabAngles               = list(rayAngles(math.radians(FOV_IN_DEGREES), NUMBER_OF_SLICE))
+    stRayAngles             = codegen.buffer2cCode("tabRayAngles", "signed char", tabAngles)
+    print (stRayAngles)
+    for angle in range (tabAngles[0], tabAngles[-1]-1, -1):
+        idx = rayAngle2Col (angle*math.pi/128, math.radians(FOV_IN_DEGREES), NUMBER_OF_SLICE)
+        print (angle, 
+            idx ,
+            tabAngles [idx]
+        )
     
 if __name__ == "__main__":
     main()

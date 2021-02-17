93467d2d28709d2432854005817a93cd4a9d42eb
Jean-Baptiste PERIN <jean-baptiste.perin@orange.com>
2021-02-17 16:16:45 +0100

    prepare clean code

--- build.bat
+++ build.bat
@@ -1,9 +1,20 @@
 
 
+REM [ref lookup_generate]
 python tools\precalDistance2Height.py > proto\c\tabunlogd2hh.h
+python tools\precalcTabMulti.py > proto\c\tabMulti.h
 python tools\precalcUnfish.py > proto\c\tabunfish.h
 python tools\precalcLogTrigo.py > proto\c\tablogtrigo.h
 python tools\precalcRayAngle.py > proto\c\tabrayangle.h
+
+REM [ref texel_codec]
 python tools\precalcTexel.py > proto\c\tabTexelColor.h
-python tools\precalcTabMulti.py > proto\c\tabMulti.h
+
+REM [ref config_generate]
 python tools\generateConstants.py > proto\c\constants.h
+
+REM [ref texture_file2buffer]
+python tools\texture2buf.py img\bluestone.png > proto\c\texture_bluestone.h
+python tools\texture2buf.py img\logo.png > proto\c\texture_logo.h
+python tools\texture2buf.py img\redbrick.png > proto\c\texture_redbrick.h
+python tools\texture2buf.py img\christmas.bmp > proto\c\texture_christmas.h

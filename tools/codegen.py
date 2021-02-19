a7a03a99434b467779d85e2b2e43173297d18e2b
Jean-Baptiste PERIN <jean-baptiste.perin@orange.com>
2021-02-19 16:15:12 +0100

    clean and fix codegen

--- tools/codegen.py
+++ tools/codegen.py
@@ -1,5 +1,5 @@
 
-NUMBER_OF_SLICE         = 80
+import config 
 
 def buffer2cCode(theName, theType, theBuffer):
     theCode = ""
@@ -31,48 +31,21 @@ def buffer2pyCode(theName, theBuffer):
     theCode += "]\n"
     return theCode
 
-def dda(nbVal, nbStep, startValue = 0):
-    ii=startValue
-    yield ii    
-    if nbVal>nbStep:
- 
-        err = nbVal
-        while (ii < nbVal+ startValue):
-            err     -= nbStep
-            ii      += 1
-            if (2*err < nbStep):
-                err     += nbVal
-                yield ii # if (ii <= nbVal+ startValue): 
-                
-    elif nbVal<nbStep:
-        
-        err = nbStep
-        while (ii < nbVal+ startValue):
-            err -= nbVal
-            if (2*err < nbStep):
-                err     += nbStep
-                ii      += 1
-            yield ii
-    else:
-        while (ii <= (nbVal + startValue)):
-            ii      += 1
-            yield ii
-COEFF = 32/6 # FIXME make global to avoid duplicate definition
+COEFF = config.TEXTURE_SIZE/ config.WALL_HEIGHT 
 def main ():
-    stWall = buffer2cCode("idxWall", "unsigned char", [0 for ii in range(NUMBER_OF_SLICE)])
-    stHeight = buffer2cCode("tabHeight", "unsigned char", [40-ii for ii in dda(28,40)])
+    stWall = buffer2cCode("idxWall", "unsigned char", [0 for ii in range(config.NUMBER_OF_SLICE)])
     stTexCol = buffer2cCode("tabTexCol", "unsigned char", [ii for ii in range(40)])
     stMulti40 = buffer2cCode("multi40", "unsigned int", [40*ii for ii in range(200)])
     stMulti120 = buffer2cCode("multi120", "unsigned int", [120*ii for ii in range(64)])
     stMulti32 = buffer2cCode("multi32", "unsigned int", [32*ii for ii in range(32)])
     stMultiCoeff = buffer2cCode("multiCoeff", "unsigned char", [round(COEFF*ii) for ii in range(45)])
     print (stWall)
-    print (stHeight)
     print (stTexCol)
     print (stMulti40)
     print (stMulti120)
     print (stMulti32)
     print (stMultiCoeff)
+
 if __name__ == "__main__":
     # execute only if run as a script
     main()    

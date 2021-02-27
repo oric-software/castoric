4f2bca6529fa317e05b75e1555ec5f61456a8feb
Jean-Baptiste PERIN <jean-baptiste.perin@orange.com>
2021-02-27 17:50:09 +0100

    resize viewport

--- proto/c/constants.h
+++ proto/c/constants.h
@@ -1,16 +1,16 @@
 // Generated file. Do not modify by hand.
 // To change values, edit config.py and regenerate with:
 // python tools\generateConstants.py > proto\c\constants.h
-#define NUMBER_OF_SLICE                 78
+#define NUMBER_OF_SLICE                 64
 
 // [ref config_viewport]
 
 #define VIEWPORT_HEIGHT                 64
-#define VIEWPORT_WIDTH                  78
-#define VIEWPORT_START_COLUMN           2
+#define VIEWPORT_WIDTH                  64
+#define VIEWPORT_START_COLUMN           6
 #define VIEWPORT_START_LINE             0
 #define TEXTURE_SIZE                    32
-#define HALF_FOV_FIX_ANGLE              40
+#define HALF_FOV_FIX_ANGLE              28
 #define EMPTY_ALPHA                     0
 
 // [ref scene_describe]

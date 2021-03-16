4505000cd3edfb5391bef8f60a95897b9996b79f
Jean-Baptiste PERIN <jean-baptiste.perin@orange.com>
2021-03-16 20:50:01 +0100

    refine collision detection

--- proto/c/player.c
+++ proto/c/player.c
@@ -53,8 +53,8 @@ void player () {
 // Collision Detection 
 unsigned char isAllowedPosition(signed char X, signed char Y) {
 
-    // return (abs(X) <= 4) && (abs(Y) <= 4);
-    return 1;
+    return (abs(X) <= 5) && (abs(Y) <= 5);
+    //return 1;
 
 }
 void forward() {

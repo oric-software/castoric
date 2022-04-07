5301031f1ce94a40c7145c7d1332951c3b530646
Jean-Baptiste PERIN <jean-baptiste.perin@orange.com>
2022-04-07 21:02:14 +0200

    lighten keyboard

--- proto/c/keyboard_c.c
+++ proto/c/keyboard_c.c
@@ -37,31 +37,31 @@ char keyQuit            = KEY_ESCAPE;
 unsigned char keybconfig        = 0; // 0 : ArrowKeys + XC,  1: AZEQSD , 2 : TYUGHJ
 unsigned char soundenabled      = 0; // 0 : No Sound, 1 = Sound Enabled
 
-void setKeyboardConfig(){
-    if (keybconfig == 0) {
-        keyForward         = KEY_UP;
-        keyBackward        = KEY_DOWN;
-        keyTurnLeft        = KEY_LEFT;
-        keyTurnRight       = KEY_RIGHT;
-        keyStraffeLeft     = KEY_X;
-        keyStraffeRight    = KEY_C;
-        keyQuit            = KEY_ESCAPE;
-    } else if (keybconfig == 1) {
-        keyForward         = KEY_Z;
-        keyBackward        = KEY_S;
-        keyTurnLeft        = KEY_A;
-        keyTurnRight       = KEY_E;
-        keyStraffeLeft     = KEY_Q;
-        keyStraffeRight    = KEY_D;
-        keyQuit            = KEY_ESCAPE;
-    } else if (keybconfig == 2) {
-        keyForward         = KEY_Y;
-        keyBackward        = KEY_H;
-        keyTurnLeft        = KEY_T;
-        keyTurnRight       = KEY_U;
-        keyStraffeLeft     = KEY_G;
-        keyStraffeRight    = KEY_J;
-        keyQuit            = KEY_ESCAPE;
-    }
-}
+// void setKeyboardConfig(){
+//     if (keybconfig == 0) {
+//         keyForward         = KEY_UP;
+//         keyBackward        = KEY_DOWN;
+//         keyTurnLeft        = KEY_LEFT;
+//         keyTurnRight       = KEY_RIGHT;
+//         keyStraffeLeft     = KEY_X;
+//         keyStraffeRight    = KEY_C;
+//         keyQuit            = KEY_ESCAPE;
+//     } else if (keybconfig == 1) {
+//         keyForward         = KEY_Z;
+//         keyBackward        = KEY_S;
+//         keyTurnLeft        = KEY_A;
+//         keyTurnRight       = KEY_E;
+//         keyStraffeLeft     = KEY_Q;
+//         keyStraffeRight    = KEY_D;
+//         keyQuit            = KEY_ESCAPE;
+//     } else if (keybconfig == 2) {
+//         keyForward         = KEY_Y;
+//         keyBackward        = KEY_H;
+//         keyTurnLeft        = KEY_T;
+//         keyTurnRight       = KEY_U;
+//         keyStraffeLeft     = KEY_G;
+//         keyStraffeRight    = KEY_J;
+//         keyQuit            = KEY_ESCAPE;
+//     }
+// }
 

1f0501d6d6156e4eb23abae40a60b2a23c8a8b3a
Jean-Baptiste PERIN <jean-baptiste.perin@orange.com>
2022-04-07 20:59:40 +0200

    asm version of engInitObjects

--- proto/c/engine_s.s
+++ proto/c/engine_s.s
@@ -17,5 +17,40 @@ EndIfObjectIsActive
 engPulseEndLoop
 .)
     rts
+
+
+_engInitObjects
+.(
+    ldy #OBJECTS_MAX: sty _engCurrentObjectIdx
+engInitLoop
+        ldy _engCurrentObjectIdx
+        beq engInitEndLoop
+        dey : sty _engCurrentObjectIdx
+        lda #0
+        sta _objActive, y
+        jmp engInitLoop
+engInitEndLoop
+.)
+    rts
+
+
+;; _engAddObject
+;; .(
+;;     lda #0
+;;     sta _engCurrentObjectIdx
+;; addObjectLoop
+;;     sta _engCurrentObjectIdx
+;;     cmp #OBJECTS_MAX
+;;     beq engAddObjectDone
+;;     tay 
+;;     lda _objActive,y
+;;     beq foundafreeslot
+;;     jmp addObjectLoop
+;; foundafreeslot
+;;     lda #1 : sta _objActive,y
+;;     
+;; engAddObjectDone
+;; .)
+;;     rts
 #endif ;; USE_C_ENGINEPULSE
 
